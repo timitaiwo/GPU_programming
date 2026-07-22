d4b2e39e9fec219e7051ae57945b22aa45966b7f
Timi Taiwo <ttaiwo73@gmail.com>
2026-07-22 09:59:34 +0100

    update infra

--- infrastructure_as_code/main.tf
+++ infrastructure_as_code/main.tf
@@ -1,3 +1,23 @@
-resource "runpod_pod" "main_node" {
-  
+resource "runpod_pod" "h100_instella_pod" {
+  compute_type = "GPU"
+
 }
+
+resource "runpod_network_volume" "weight_storer" {
+  name = "weight-storer"
+  data_center_id = "EUR-NO-2"
+  size = 1000
+}
+
+
+// gpu type 
+// name
+// pod template - create custom
+// count
+
+// ssh_name 
+// ssh_login command
+
+
+// Network storage is S3 compatible so weights can be sent to the storage before the pod is started
+
--- infrastructure_as_code/variables.tf
+++ infrastructure_as_code/variables.tf
@@ -2,3 +2,16 @@ variable "runpod_api_key" {
   type = string
   sensitive = true
 }
+
+variable "h100_type"{
+    description = "The gpu type e.g H100"
+    type = string
+    default = "h100"
+} 
+
+variable "h100_template" {
+  description = "Template to apply to the gpu pod"
+  type = string
+  default = "runpod/pytorch:1.0.2-cu1281-torch280-ubuntu2404"
+}
+
